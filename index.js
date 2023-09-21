30844ce3a76c73822c2420e2ec90e1b849b45c97
byron korir <byron.korir@student.moringaschool.com>
2023-09-21 09:42:52 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = "bob"
+function upperCaseCustomerName(){
+    return customerName =customerName.toUpperCase()
+    
+}
+function setBestCustomer(){
+    return bestCustomer = `not bob`
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = ""
+function changeLeastFavoriteCustomer(){
+  return leastFavoriteCustomer = 'test'
+}
